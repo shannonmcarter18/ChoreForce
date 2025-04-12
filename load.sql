20d19bb6a763ffd06a960134f43306a8f073321c
Gayathri Utla <gayathri.utla2216@gmail.com>
2025-04-12 18:43:12 -0500

    Update load.sql

--- load.sql
+++ load.sql
@@ -1,28 +1,28 @@
 
-LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/parents.csv'
+LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/PARENT.csv'
 INTO TABLE PARENT
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/children.csv'
+LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/CHILD.csv'
 INTO TABLE CHILD
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/user.csv'
+LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/USER.csv'
 INTO TABLE USER_ACCOUNT
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/chores.csv'
+LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/CHORE.csv'
 INTO TABLE CHORE
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/parents.csv'
+LOAD DATA INFILE '/Users/gayathriutla/Desktop/Projects/databases_class_project/PAYMENT.csv'
 INTO TABLE PAYMENT
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
