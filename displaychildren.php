74b894b6c6c50af4a98402a4e0e687da6bd23264
Shannon Carter <shannon.m.carter.18@gmail.com>
2025-04-26 10:54:41 -0500

    working addchore + fixed navbar routing to parentportal.php on files

--- displaychildren.php
+++ displaychildren.php
@@ -4,14 +4,9 @@ session_start();
 $parent_id = $_SESSION['user_id'] ?? null;
 
 if (!$parent_id) {
-    echo $parent_id; 
     echo "No user session found.";
-    //echo "<script>console.log('No user session found.');</script>";
     exit();
 }
-
-echo $parent_id; 
-echo "Hello this is shannon"; 
 
 $db_host = "localhost";
 $db_user = "root";
@@ -23,15 +18,23 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT id, name FROM child WHERE parent_id = ?";
+$sql = "SELECT child.CID, user.FIRST_NAME, user.LAST_NAME
+        FROM CHILD
+        JOIN USER ON child.ID = user.ID
+        WHERE child.PID = ?";
 $stmt = mysqli_prepare($conn, $sql);
+if (!$stmt) {
+    die("SQL error: " . mysqli_error($conn));
+}
+
 mysqli_stmt_bind_param($stmt, "i", $parent_id);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 
 while ($row = mysqli_fetch_assoc($result)) {
-    echo '<input type="radio" id="child_' . $row['id'] . '" name="chore_assign" value="' . $row['id'] . '" required>';
-    echo '<label for="child_' . $row['id'] . '">' . htmlspecialchars($row['name']) . '</label><br>';
+    $full_name = htmlspecialchars($row['FIRST_NAME'] . ' ' . $row['LAST_NAME']);
+    echo '<input type="radio" id="child_' . $row['CID'] . '" name="chore_assign" value="' . $row['CID'] . '" required>';
+    echo '<label for="child_' . $row['CID'] . '">' . $full_name . '</label><br>';
 }
 
 mysqli_stmt_close($stmt);
